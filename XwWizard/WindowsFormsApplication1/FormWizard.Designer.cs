c0b550a753a9f533e0b69404a8915ace913ffb2e
LYF <lyfjxymf@sina.com>
2014-11-08 15:27:27 +0800

    Working on the wizard (怎么最近Bug这么多)

--- XwWizard/WindowsFormsApplication1/FormWizard.Designer.cs
+++ XwWizard/WindowsFormsApplication1/FormWizard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWizard));
             this.btnNext = new System.Windows.Forms.Button();
             this.btnPrev = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
@@ -91,6 +92,7 @@
             this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.btnNext);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormWizard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
